54e6734db5ab791685115c20cf7e392400a4f83e
Gonzalo Cabrera <168502931+gonzalocabreravaras@users.noreply.github.com>
2024-06-08 19:20:09 -0400

    Implementación menú burger

---
+++ js/scrip.js
@@ -0,0 +1,61 @@
+/**HEADER STICKY*/
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementById('header');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) { // Ajusta el valor de desplazamiento según sea necesario
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
+});
+
+
+
+/**MENÚ BURGER*/
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    // Función para resaltar el enlace activo
+    function highlightActiveLink(event) {
+        navLinks.forEach(link => {
+            link.classList.remove('active'); // Removemos la clase 'active' de todos los enlaces
+        });
+        event.target.classList.add('active'); // Agregamos la clase 'active' solo al enlace clickeado
+    }
+
+    // Listener para resaltar el enlace activo al hacer clic en un enlace
+    navLinks.forEach(link => {
+        link.addEventListener('click', highlightActiveLink);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            // Removemos la clase 'active' de todos los enlaces
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            // Agregamos la clase 'active' solo al enlace que se hizo clic
+            this.classList.add('active');
+        });
+    });
+});
+
