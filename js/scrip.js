8207f7c4d9171cb57d75373caa3efed7eeed1527
Gonzalo Cabrera <168502931+gonzalocabreravaras@users.noreply.github.com>
2024-06-10 18:18:35 -0400

    SEO

--- js/scrip.js
+++ js/scrip.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const header = document.getElementById('header');
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) { // Ajusta el valor de desplazamiento según sea necesario
+        if (window.scrollY > 50) {
             header.classList.add('fixed');
         } else {
             header.classList.remove('fixed');
@@ -30,12 +30,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // Función para resaltar el enlace activo
     function highlightActiveLink(event) {
         navLinks.forEach(link => {
-            link.classList.remove('active'); // Removemos la clase 'active' de todos los enlaces
+            link.classList.remove('active');
         });
-        event.target.classList.add('active'); // Agregamos la clase 'active' solo al enlace clickeado
+        event.target.classList.add('active');
     }
 
-    // Listener para resaltar el enlace activo al hacer clic en un enlace
     navLinks.forEach(link => {
         link.addEventListener('click', highlightActiveLink);
     });
@@ -46,12 +45,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     navLinks.forEach(link => {
         link.addEventListener('click', function() {
-            // Removemos la clase 'active' de todos los enlaces
             navLinks.forEach(link => {
                 link.classList.remove('active');
             });
 
-            // Agregamos la clase 'active' solo al enlace que se hizo clic
             this.classList.add('active');
         });
     });
@@ -87,10 +84,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
         if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
-            // Scroll hacia abajo, ocultar el header
             header.classList.add('hide');
         } else {
-            // Scroll hacia arriba o en la parte superior, mostrar el header
             header.classList.remove('hide');
         }
 
@@ -100,33 +95,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.getElementById("myForm").addEventListener("submit", function(event) {
-    // Evitar el envío por defecto del formulario
     event.preventDefault();
 
-    // Cambiar el texto del botón y desactivarlo
     var submitButton = document.getElementById("submitButton");
     submitButton.innerHTML = "Enviando mensaje...";
     submitButton.disabled = true;
     submitButton.classList.add("btn-enviando");
-    submitButton.style.opacity = "0.5"; // Asegurar que la opacidad sea 1
+    submitButton.style.opacity = "0.5";
 
-    // Obtener los datos del formulario
     var formData = new FormData(this);
 
-    // Realizar una solicitud AJAX
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://formspree.io/f/xoqggvpo", true);
     xhr.setRequestHeader("Accept", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            // Después de 2 segundos, cambiar el texto a "Mensaje enviado" y cambiar el estilo
             setTimeout(function() {
                 submitButton.innerHTML = "Mensaje enviado 🥳";
                 submitButton.classList.remove("btn-enviando");
                 submitButton.classList.add("btn-enviado");
-                submitButton.style.opacity = "1"; // Asegurar que la opacidad sea 1
+                submitButton.style.opacity = "1";
 
-                // Después de 3 segundos, redirigir al usuario a la página deseada
                 setTimeout(function() {
                     window.location.href = "https://gonzalocabreravaras.github.io/gonzalocabrera/";
                 }, 3000);
